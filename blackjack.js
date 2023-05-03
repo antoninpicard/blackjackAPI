4aa52b551e6687c94bdf64371da27629ddc8f59a
Antonin <antoninpicard0@gmail.com>
2023-05-03 14:02:16 +0200

    optimus

--- blackjack.js
+++ blackjack.js
@@ -1,211 +1,248 @@
-// Déclarer deck_id comme variable globale
-let deckId;
-let playerScore = 0;
-let dealerScore = 0;
-let isPlayer = true;
-let cards;
-let playerCards = [];
-let dealerCards = [];
+// Variables globales
+let deckId, playerScore, dealerScore, playerCards, dealerCards;
+playerScore = dealerScore = 0;
+playerCards = [];
+dealerCards = [];
 const playerCardsDiv = document.getElementById("player-cards-container");
 const dealerCardsDiv = document.getElementById("dealer-cards-container");
+let balance = 3000;
+let bet = 0;
 
 // Créer un nouveau deck mélangé
-function ShuffleCards() {
-  fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      deckId = data.deck_id;
-      playerScore = 0;
-      dealerScore = 0;
-      document.getElementById("hit-button").disabled = false;
-      document.getElementById("stand-button").disabled = false;
-      document.getElementById("deal-button").innerText = "Reset";
-      document
-        .getElementById("deal-button")
-        .setAttribute("onclick", "location.reload()");
-      document.getElementById("player-score").innerText = "Score: 0";
-      document.getElementById("dealer-score").innerText = "Score: 0";
-      DrawCards();
-    })
-    .catch((error) => console.error(error));
+async function shuffleCards() {
+  try {
+    const response = await fetch(
+      "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6"
+    );
+    const data = await response.json();
+    deckId = data.deck_id;
+
+    resetUI();
+    drawCards();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // Distribution des cartes
-function DrawCards() {
-  fetch("https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=4")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      // Afficher les cartes du joueur
-      for (let i = 0; i < 2; i++) {
-        const cardValue = getValue(data.cards[i].value, true);
-        playerScore += cardValue;
-        playerCards.push({ value: data.cards[i].value, score: cardValue }); // Ajoutez cette ligne pour stocker les cartes du joueur
-        document.getElementById("player-score").innerText = "Me: " + playerScore;
-      }
-      
-      
-      for (let i = 2; i < 4; i++) {
-        const cardValue = getValue(data.cards[i].value, false); // Passer 'false' pour indiquer que c'est pour le dealer
-        dealerScore += cardValue;
-        dealerCards.push({ value: data.cards[i].value, score: cardValue }); // Ajouter cette ligne pour stocker les cartes du dealer
-        document.getElementById("dealer-score").innerText =
-          "Dealer: " + dealerScore;
-      }
-      
-      // Affichage des cartes du dealer
-      cards = document.createElement("img");
-      cards.src = data.cards[2].images.png;
-      dealerCardsDiv.appendChild(cards);
-      
-      cards = document.createElement("img");
-      cards.src = data.cards[3].images.png;
-      dealerCardsDiv.appendChild(cards);
-      
+async function drawCards() {
+  try {
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`
+    );
+    const data = await response.json();
 
-
-      //affichage cards player
-      cards = document.createElement("img");
-      cards.src = data.cards[0].images.png;
-      playerCardsDiv.appendChild(cards);
-
-      cards = document.createElement("img");
-      cards.src = data.cards[1].images.png;
-      playerCardsDiv.appendChild(cards);
-
-
-
-      
-    })
-    .catch((error) => console.error(error));
+    displayCards(data, 0, 2, true);
+    displayCards(data, 2, 4, false);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // Piocher une carte
-function Hit() {
-  fetch("https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=1")
-    .then((response) => response.json())
-    .then((data) => {
-      const cardValue = getValue(data.cards[0].value, true);
-      playerScore += cardValue;
-      playerCards.push({ value: data.cards[0].value, score: cardValue });
-      
+async function hit() {
+  try {
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+    );
+    const data = await response.json();
 
-      //affichage carte quand on hit
-      cards = document.createElement("img");
-      cards.src = data.cards[0].images.png;
-      playerCardsDiv.appendChild(cards);
+    const cardValue = getValue(data.cards[0].value, true);
+    playerScore += cardValue;
+    playerCards.push({ value: data.cards[0].value, score: cardValue });
 
-      if (playerScore > 21) {
-        for (let i = 0; i < playerCards.length; i++) {
-          if (playerCards[i].value === "ACE" && playerCards[i].score === 11) {
-            playerScore -= 10; // Changez la valeur de l'As de 11 à 1
-            playerCards[i].score = 1;
-            break;
-          }
-        }
-      }
-    
-      document.getElementById("player-score").innerText = "Me: " + playerScore;
+    updateScore("player-score", playerScore, "Me");
+    displayCard(data, 0, playerCardsDiv);
 
-      if (playerScore > 21) {
-        setTimeout(determineWinner, 500);
-      }
-    })
-    .catch((error) => console.error(error));
+    checkBust(playerCards, playerScore, "player-score", "Me", determineWinner);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-// Permet de mettre à jour le score du dealer
-function DealerScore(numbers) {
-  const dealerScoreElement = document.getElementById("dealer-score");
-  dealerScoreElement.innerText = "Dealer: " + dealerScore;
+// Dealer hit
+async function dealerHit() {
+  try {
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+    );
+    const data = await response.json();
+
+    const cardValue = getValue(data.cards[0].value, false);
+    dealerScore += cardValue;
+    dealerCards.push({ value: data.cards[0].value, score: cardValue });
+
+    updateScore("dealer-score", dealerScore, "Dealer");
+    displayCard(data, 0, dealerCardsDiv);
+
+    if (dealerScore < 17) {
+      dealerHit();
+    } else {
+      setTimeout(determineWinner, 1000);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
+function stand() {
+  disableButtons();
+  dealerScore < 17 ? dealerHit() : determineWinner();
+}
 
+function resetGame() {
+  resetScores();
+  resetCards();
+  enableButtons();
 
+  bet = 0;
+  document.getElementById("bet").value = 0;
+  document.getElementById("deal-button").innerText = "Deal";
+  document.getElementById("deal-button").setAttribute("onclick", "placeBet()");
+}
 
-// Fonction pour retourner la valeur numérique d'une carte
+function placeBet() {
+  bet = parseInt(document.getElementById("bet").value);
+
+  if (bet > 0 && bet <= balance) {
+    updateBalance(-bet);
+    disableBetInput();
+    shuffleCards();
+  } else {
+    bet > balance
+      ? alert("You cannot bet more than your current balance.")
+      : alert("Invalid bet. Please enter a valid amount.");
+  }
+}
+
+function determineWinner() {
+  disableButtons();
+  const [payout, message] = calculatePayout();
+  updateBalance(payout);
+  enableBetInput();
+  alert(message);
+}
+
+// Fonctions utilitaires
 function getValue(cardValue, isPlayerCard) {
   if (cardValue === "ACE") {
     let score = isPlayerCard ? playerScore : dealerScore;
-    if (score + 11 > 21) {
-      return 1;
-    } else {
-      return 11;
+    return score + 11 > 21 ? 1 : 11;
+  }
+  if (["KING", "QUEEN", "JACK"].includes(cardValue)) return 10;
+  return parseInt(cardValue);
+}
+
+function displayCards(data, start, end, isPlayer) {
+  for (let i = start; i < end; i++) {
+    const cardValue = getValue(data.cards[i].value, isPlayer);
+    const score = isPlayer
+      ? (playerScore += cardValue)
+      : (dealerScore += cardValue);
+    const cards = isPlayer ? playerCards : dealerCards;
+    cards.push({ value: data.cards[i].value, score: cardValue });
+
+    const scoreElement = isPlayer ? "player-score" : "dealer-score";
+    const scorePrefix = isPlayer ? "Me" : "Dealer";
+    updateScore(scoreElement, score, scorePrefix);
+
+    const cardsDiv = isPlayer ? playerCardsDiv : dealerCardsDiv;
+    displayCard(data, i, cardsDiv);
+  }
+}
+
+function displayCard(data, index, cardsDiv) {
+  const card = document.createElement("img");
+  card.src = data.cards[index].images.png;
+  cardsDiv.appendChild(card);
+}
+
+function checkBust(cards, score, scoreElement, scorePrefix, callback) {
+  if (score > 21) {
+    for (let card of cards) {
+      if (card.value === "ACE" && card.score === 11) {
+        score -= 10;
+        card.score = 1;
+        break;
+      }
     }
-  } else if (
-    cardValue === "KING" ||
-    cardValue === "QUEEN" ||
-    cardValue === "JACK"
-  ) {
-    return 10;
-  } else {
-    return parseInt(cardValue);
+    updateScore(scoreElement, score, scorePrefix);
+    if (score > 21) setTimeout(callback, 500);
   }
 }
 
-function Stand() {
-  document.getElementById("hit-button").disabled = true;
-  document.getElementById("stand-button").disabled = true;
-  if (dealerScore < 17) {
-    DealerHit();
-  } else {
-    determineWinner();
-  }
+function updateScore(elementId, score, prefix) {
+  document.getElementById(elementId).innerText = `${prefix}: ${score}`;
 }
 
-function DealerHit() {
-  fetch("https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=1")
-    .then((response) => response.json())
-    .then((data) => {
-      const cardValue = getValue(data.cards[0].value, false); // Passer 'false' pour indiquer que c'est pour le dealer
-      dealerScore += cardValue;
-      dealerCards.push({ value: data.cards[0].value, score: cardValue }); // Ajouter cette ligne pour stocker les cartes du dealer
-      // Affichage de la carte supplémentaire du dealer
-      cards = document.createElement("img");
-      cards.src = data.cards[0].images.png;
-      dealerCardsDiv.appendChild(cards);
-      if (dealerScore > 21) {
-        for (let i = 0; i < dealerCards.length; i++) {
-          if (dealerCards[i].value === "ACE" && dealerCards[i].score === 11) {
-            dealerScore -= 10; // Changez la valeur de l'As de 11 à 1
-            dealerCards[i].score = 1;
-            break;
-          }
-        }
-      }
-
-      document.getElementById("dealer-score").innerText =
-        "Dealer: " + dealerScore;
-      if (dealerScore < 17) {
-        DealerHit();
-      } else {
-        setTimeout(() => {
-          //le setTimeout permet de mettre à jour le score et l'affichage des cartes avant les alerts de résulat
-          determineWinner();
-        }, 1000);
-      }
-    })
-    .catch((error) => console.error(error));
+function resetScores() {
+  playerScore = dealerScore = 0;
+  updateScore("player-score", playerScore, "Me");
+  updateScore("dealer-score", dealerScore, "Dealer");
 }
 
+function resetCards() {
+  playerCards = [];
+  dealerCards = [];
+  playerCardsDiv.innerHTML = "";
+  dealerCardsDiv.innerHTML = "";
+}
 
-
-
-function determineWinner() {
+function disableButtons() {
   document.getElementById("hit-button").disabled = true;
   document.getElementById("stand-button").disabled = true;
+}
+
+function enableButtons() {
+  document.getElementById("hit-button").disabled = false;
+  document.getElementById("stand-button").disabled = false;
+}
+
+function updateBalance(amount) {
+  balance += amount;
+  document.getElementById("balance").innerText = "Balance: " + balance;
+}
+
+function disableBetInput() {
+  document.getElementById("bet").disabled = true;
+}
+
+function enableBetInput() {
+  document.getElementById("bet").disabled = false;
+}
+
+function calculatePayout() {
+  let payout = 0;
+  let message = "";
 
   if (playerScore > 21) {
-    alert("You Bust, Dealer Win !!");
+    message = "You Bust, Dealer Win !!";
   } else if (dealerScore > 21) {
-    alert("Dealer Bust, Player Win !!");
+    payout = bet * 2;
+    message = "Dealer Bust, Player Win !!";
   } else if (playerScore === dealerScore) {
-    alert("It's a Tie!");
+    payout = bet;
+    message = "It's a Tie!";
   } else if (playerScore > dealerScore) {
-    alert("Player Wins!");
+    if (playerScore === 21 && playerCards.length === 2) {
+      payout = bet * 2.5;
+      message = "Blackjack! Player Wins!";
+    } else {
+      payout = bet * 2;
+      message = "Player Wins!";
+    }
   } else {
-    alert("Dealer Wins!");
+    message = "Dealer Wins!";
   }
+
+  return { payout, message };
 }
 
+// Fonction pour initialiser le jeu
+function init() {
+  resetGame();
+  document.getElementById("balance").innerText = "Balance: " + balance;
+  document.getElementById("bet").value = 0;
+}
+
+init();
